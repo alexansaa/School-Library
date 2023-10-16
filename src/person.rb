fc4e5f15d50b5e090580e2f3f2a42d84bfadfd74
alexansaa <alexan.saa@gmail.com>
2023-10-16 15:10:38 -0500

    Fix linter errors

--- src/person.rb
+++ src/person.rb
@@ -1,9 +1,5 @@
 require_relative 'nameable'
 
-require_relative 'capitalize_decorator'
-
-require_relative 'trimmer_decorator'
-
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
@@ -28,10 +24,3 @@ class Person < Nameable
     @name
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
--- trimmer_decorator.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'decorator'
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable[0..9]
-  end
-end

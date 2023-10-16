b0130abc606f79a252ff4a2492d50028751d12ad
alexansaa <alexan.saa@gmail.com>
2023-10-16 13:12:09 -0500

    implemented decorators and test

---
+++ nameable.rb
@@ -0,0 +1,8 @@
+class Nameable
+  public
+  
+  def correct_name
+    raise NotImplementedError, "Subclasses must implement the correct_name method"
+  end
+end
+  
--- person.rb
+++ person.rb
@@ -1,4 +1,10 @@
-class Person
+require_relative 'nameable'
+
+require_relative 'capitalize_decorator'
+
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
@@ -13,9 +19,18 @@ class Person
     of_age? || @parent_permission
   end
 
-  private
-
   def of_age?
     @age >= 18
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
